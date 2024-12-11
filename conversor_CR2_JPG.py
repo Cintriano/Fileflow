77f65c235d8d3a3586e09f72e11ad3643a7ea19e
danilo <danisback@rede.ulbra.br>
2024-12-11 18:08:50 -0300

    revisão das funções converter e excluir

--- conversor_CR2_JPG.py
+++ conversor_CR2_JPG.py
@@ -10,18 +10,14 @@ def converter_cr2_jpg(pasta):
                 novo_nome = arquivo.replace(".CR2", ".jpg")
                 novo_caminho = os.path.join(pasta, novo_nome)
                 img.save(novo_caminho, "JPEG")
-                os.remove(caminho_completo)
-                print(f"Arquivo convertido e excluído: {novo_caminho}")
             except Exception as e:
-                print(f"Erro ao converter ou excluir {arquivo}: {e}")
+                print(f"Erro Função(converter_cr2_jpg): {e}")
 
 def excluir_cr2(pasta):
-    for arquivo in os.listdir(pasta):
-        if arquivo.endswith(".CR2"):
-            caminho_completo = os.path.join(pasta, arquivo)
-            os.remove(caminho_completo)
-
-
-caminho_pasta = r"C:\Users\danil\OneDrive\Teste"
-converter_cr2_jpg(caminho_pasta)
-excluir_cr2(caminho_pasta)
+    try:
+        for arquivo in os.listdir(pasta):
+            if arquivo.endswith(".CR2"):
+                caminho_completo = os.path.join(pasta, arquivo)
+                os.remove(caminho_completo)
+    except Exception as e:
+        print(f"Erro Função(excluir_cr2): {e}")
