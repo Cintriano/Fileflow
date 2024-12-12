e98de37b463be28a675c14b0d70a121dfc9b20c9
danilo <danisback@rede.ulbra.br>
2024-12-12 19:07:50 -0300

    revisando o log adicionando o numero de execuções e tipo

--- log.py
+++ log.py
@@ -5,9 +5,10 @@ from rename import validacao_arq
 
 
 #CRIA REGISTROS DE TODAS AS EXECUÇÕES
-def log(infos):
+def log(infos, tipo):
     pasta = r"C:\Users\danil\OneDrive\Temporários\Teste\Log"
     num = random.randint(10000, 99999)
+    quant_arq = len(infos)
     data = datetime.now().strftime('%d.%m.%Y')
     hora = datetime.now().strftime("%H:%M:%S")
     nome_arquivo = f"LOG_{data}_{num}.txt"
@@ -17,15 +18,13 @@ def log(infos):
             if data == arquivo[4:14]:
                 caminho_arquivo = os.path.join(pasta, arquivo)
                 with open(caminho_arquivo, 'a') as relatorio:
-                    relatorio.write('==========================\n')
-                    relatorio.write(f'Execução/{data}/{hora}\n')
+                    relatorio.write(f'Execução/{data}/{hora}/{quant_arq}/{tipo}\n')
                     for info in infos:
                         linha = f'{info[0]}/{info[1]}/{info[2]}\n'
                         relatorio.write(linha)
                     return True
         with open(caminho_arquivo, 'a') as relatorio:
-            relatorio.write('==========================\n')
-            relatorio.write(f'Execução/{data}/{hora}\n')
+            relatorio.write(f'Execução/{data}/{hora}/{quant_arq}/{tipo}\n')
             for info in infos:
                 linha = f'{info[0]}/{info[1]}/{info[2]}\n'
                 relatorio.write(linha)
--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from log import *
 #FUNÇÃO PRINCIPAL DE RENOMEAÇÃO DE ARQUIVOS, RESPONSAVEL POR VALIDAR E DELEGAR OS ARQUIVOS
 def main():
     pasta = r"C:\Users\danil\OneDrive\Temporários\Teste"
+    #pasta = r"D:\DCIM\100CANON"
     infos = []
+
+    if not os.path.exists(pasta):
+        return "Pasta não Existente"
+
     try:
         for arquivo in os.listdir(pasta):
             caminho_arquivo = os.path.join(pasta, arquivo)
@@ -15,8 +20,8 @@ def main():
                     infos.append((lista[1], arquivo, lista[2]))
                 else:
                     infos = infos + (renomear_nome(arquivo, pasta))
-                if len(infos) != 0:
-                    log(infos)
+        if len(infos) != 0:
+            log(infos, "r")
     except Exception as e:
         print(f"Erro ao processar {arquivo}: {e}")
     return "Processo Finalizado"
