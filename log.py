eada78c783c82a893c8437c790b4d366998b5731
danilo <danisback@rede.ulbra.br>
2024-12-24 18:49:48 -0300

    requirements e log atualizado

--- log.py
+++ log.py
@@ -8,7 +8,6 @@ from rename import validacao_arq
 def log(infos, tipo):
     pasta = r"C:\Users\danil\OneDrive\Temporários\Teste\Log"
     num = random.randint(10000, 99999)
-    quant_arq = len(infos)
     data = datetime.now().strftime('%d.%m.%Y')
     hora = datetime.now().strftime("%H:%M:%S")
     nome_arquivo = f"LOG_{data}_{num}.txt"
@@ -18,15 +17,13 @@ def log(infos, tipo):
             if data[3:10] == arquivo[7:14]:
                 caminho_arquivo = os.path.join(pasta, arquivo)
                 with open(caminho_arquivo, 'a') as relatorio:
-                    relatorio.write(f'Execução/{data}/{hora}/{quant_arq}/{tipo}\n')
                     for info in infos:
-                        linha = f'{info[0]}/{info[1]}/{info[2]}\n'
+                        linha = f'{data}/{hora}/{tipo}/{info[0]}/{info[1]}/{info[2]}\n'
                         relatorio.write(linha)
                     return True
         with open(caminho_arquivo, 'a') as relatorio:
-            relatorio.write(f'Execução/{data}/{hora}/{quant_arq}/{tipo}\n')
             for info in infos:
-                linha = f'{info[0]}/{info[1]}/{info[2]}\n'
+                linha = f'{data}/{hora}/{tipo}/{info[0]}/{info[1]}/{info[2]}\n'
                 relatorio.write(linha)
             return True
     except Exception as e:
