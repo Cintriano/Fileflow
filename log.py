c25e16cf7796a6e0f9fd06a48eac2eeadb3f2a39
Cintriano <Vdanilofernandes9077@gmail.com>
2025-06-08 18:06:34 -0300

    log novo terminado

--- log.py
+++ log.py
@@ -15,7 +15,7 @@ def log(infos, tipo):
     dic_mes = {
         '01': 'Janeiro',
         '02': 'Fevereiro',
-        '03': 'Março',
+        '03': 'Marco',
         '04': 'Abril',
         '05': 'Maio',
         '06': 'Junho',
@@ -37,21 +37,21 @@ def log(infos, tipo):
                             data_str = info[0].split('_')[0]
                             dia_captura, mes_captura, ano_captura = data_str.split('.')
                             data_captura = dia_captura + "." + mes_captura + "." + ano_captura
-                            linha = f'{data}/{hora}/{tipo}/{data_captura}/{dia_captura}/{dic_mes.get(mes_captura)}/{ano_captura}/{info[0]}/{info[1]}/{info[2]}\n'
+                            linha = f'{data};{hora};{tipo};{data_captura};{dia_captura};{dic_mes.get(mes_captura)};{ano_captura};{info[0]};{info[1]};{info[2]}\n'
                         else:
-                            linha = f'{data}/{hora}/{tipo}/0/0/0/0/{info[0]}/{info[1]}/{info[2]}\n'
+                            linha = f'{data};{hora};{tipo};0:0:0:0;{info[0]};{info[1]};{info[2]}\n'
                         relatorio.write(linha)
                     return True
         with open(caminho_arquivo, 'a') as relatorio:
-            relatorio.write("data_execucao/hora_execucao/tipo_processo/data_captura/dia_captura/mes_captura/ano_captura/nome_novo/nome_antigo/dispositivo\n")
+            relatorio.write("data_execucao;hora_execucao;tipo_processo;data_captura;dia_captura;mes_captura;ano_captura;nome_novo;nome_antigo;dispositivo\n")
             for info in infos:
                 if padrao_data(info[0]):
                     data_str = info[0].split('_')[0]
                     dia_captura, mes_captura, ano_captura = data_str.split('.')
                     data_captura = dia_captura + "." + mes_captura + "." + ano_captura
-                    linha = f'{data}/{hora}/{tipo}/{data_captura}/{dia_captura}/{dic_mes.get(mes_captura)}/{ano_captura}/{info[0]}/{info[1]}/{info[2]}\n'
+                    linha = f'{data};{hora};{tipo};{data_captura};{dia_captura};{dic_mes.get(mes_captura)};{ano_captura};{info[0]};{info[1]};{info[2]}\n'
                 else:
-                    linha = f'{data}/{hora}/{tipo}/0/0/0/0/{info[0]}/{info[1]}/{info[2]}\n'
+                    linha = f'{data};{hora};{tipo};0:0:0:0;{info[0]};{info[1]};{info[2]}\n'
                 relatorio.write(linha)
             return True
     except Exception as e:
