86d379b04b641bf9323506120e090dfad7e854af
Cintriano <danilofernandes9077@gmail.com>
2025-09-17 14:47:45 -0300

    interface iniciada

---
+++ src/interface/componentes/comp_selec_pasta.py
@@ -0,0 +1,3 @@
+def select_pasta():
+    campo_escolha_pasta = ctk.CTkLabel(janela, text="Selecione a pata de origem")
+    campo_escolha_pasta.pack()
---
+++ src/interface/janela.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk
+import comp_selec_pasta
+
+# Definindo o tema da interface
+ctk.set_appearance_mode("dark")
+
+janela = ctk.CTk() # Criando a instancia da janela
+janela.title("Teste-01") # Texto do head da janela
+janela.geometry("300x300") # Tamanho da janela
+
+
+
+janela.mainloop() # Execução da janela
