ecccd1af0486243d60a9d19269ce9000c7a46406
Cintriano <danilofernandes9077@gmail.com>
2025-09-20 14:11:43 -0300

    Configurando interface e a execução do projeto

--- src/interface/componentes/comp_selec_pasta.py
+++ src/interface/componentes/comp_selec_pasta.py
@@ -1,3 +1,5 @@
-def select_pasta():
-    campo_escolha_pasta = ctk.CTkLabel(janela, text="Selecione a pata de origem")
+import customtkinter as ctk
+
+def create_label(label: str, janela):
+    campo_escolha_pasta = ctk.CTkLabel(janela, text=label)
     campo_escolha_pasta.pack()
--- src/interface/janela.py
+++ src/interface/janela.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
-import comp_selec_pasta
+from componentes.comp_selec_pasta import *
+
+#  python -m src.interface.janela
 
 # Definindo o tema da interface
 ctk.set_appearance_mode("dark")
@@ -7,7 +9,5 @@ ctk.set_appearance_mode("dark")
 janela = ctk.CTk() # Criando a instancia da janela
 janela.title("Teste-01") # Texto do head da janela
 janela.geometry("300x300") # Tamanho da janela
-
-
 
 janela.mainloop() # Execução da janela
